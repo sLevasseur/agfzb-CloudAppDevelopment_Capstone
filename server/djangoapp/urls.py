d041609e37a37c03ce5538bfdd59e4a98744bcb6
sLevasseur <simonphilippe.levasseur@gmail.com>
2021-09-10 12:10:44 +0000

    ready deploy on ibm

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route="about/", view=views.about, name="about_us"),
     # path for contact us view
-
+    path(route="contact/", view=views.contact, name="contact_us"),
+   
     # path for registration
 
     # path for login
