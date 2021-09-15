10c3cf3ff67a55fd6c0b40bb2036e88a15ad7253
sLevasseur <simonphilippe.levasseur@gmail.com>
2021-09-15 08:42:05 +0000

    adding models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 10
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ('car_make', 'name', 'dealer_id', 'type', 'year')
+    search_fields = ['year', 'name', 'type']
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    fields = ["name", "date_created"]
+    search_fields=["name"]
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 
 
+
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
@@ -9,17 +10,34 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    date_created = models.DateField(auto_now=True)
 
+    def __str__(self):
+        return f' {self.name} : {self.description}'
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    SUV = "SUV"
+    SEDAN = "Sedan"
+    WAGON = "Wagon"
+    PEUGEOT = "Peugeot"
+    MODEL_CHOICES = [
+        (SUV, 'Suv'),
+        (SEDAN, 'Sedan'),
+        (WAGON, 'Wagon'),
+        (PEUGEOT, 'Peugeot'),
+    ]
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    dealer_id = models.IntegerField(help_text='refers to a dealer created in Cloudant database')
+    type = models.CharField(max_length=8, choices=MODEL_CHOICES, default=SUV)
+    year = models.DateField()
 
+    def __str__(self):
+        return f'{self.car_make.name} from dealer number {self.dealer_id}\
+            is a {self.type} from {self.year}'
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
